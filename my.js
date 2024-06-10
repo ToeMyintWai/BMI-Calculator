174cbe42733810907b95e091db86808382dcfca0
ToeMyintWai <76584493+ToeMyintWai@users.noreply.github.com>
2024-06-10 19:24:43 +0630

    Add files via upload
    adding metric system

--- my.js
+++ my.js
@@ -4,44 +4,60 @@ const heightInches = document.getElementById("inches");
 const weight = document.getElementById("weight");
 const resultNumber = document.getElementById("result-number");
 const resultText = document.getElementById("result-text");
+const metricSys = document.getElementById("bmi-imperial");
 
 let totalHeight = 0;
 let bmiNumber = 0;
 
+const bmiSystems = () => {
+  if (document.getElementById("bmi-imperial").checked) {
+    weight.placeholder = "lbs";
+    heightFeet.placeholder = "ft";
+    heightInches.placeholder = "in";
+    heightInches.style.display = "inline";
+    heightFeet.style.width = "45%";
+  } else {
+    weight.placeholder = "kg";
+    heightFeet.placeholder = "cm";
+    heightInches.style.display = "none";
+    heightFeet.style.width = "92%";
+  }
+};
+
 const bmiResult = () => {
   calculateBMI();
   resultNumber.innerText = bmiNumber;
-      if (bmiNumber<18.5){
-        resultText.innerHTML = `You are Underweight`
-      }
-      else if (bmiNumber >=18.5 && bmiNumber < 25) {
-        resultText.innerHTML = `Yor are Healthy<br>
+  if (bmiNumber < 18.5) {
+    resultText.innerHTML = `You are Underweight`;
+  } else if (bmiNumber >= 18.5 && bmiNumber < 25) {
+    resultText.innerHTML = `Yor are Healthy<br>
         <span>😊</span>`;
-      } else if (bmiNumber >= 25 && bmiNumber < 30) {
-        resultText.innerHTML = `You are Overweight<br>
+  } else if (bmiNumber >= 25 && bmiNumber < 30) {
+    resultText.innerHTML = `You are Overweight<br>
          <span>😥</span>`;
-      } else if (bmiNumber >= 30 && bmiNumber < 40) {
-        resultText.innerHTML= `You are Obese<br>
+  } else if (bmiNumber >= 30 && bmiNumber < 40) {
+    resultText.innerHTML = `You are Obese<br>
          <span>😰</span>`;
-      } else {
-        resultText.innerHTML = `You are Severely Obese<br>
+  } else {
+    resultText.innerHTML = `You are Severely Obese<br>
          <span>😭</span>`;
-      }
-      
-
-  
+  }
 };
 
 button.addEventListener("click", bmiResult);
 
-const addHeight = () => {
-  totalHeight = parseInt(heightFeet.value) * 12 + parseInt(heightInches.value);
-};
-
 const calculateBMI = () => {
-  addHeight();
-  bmiNumber = (
-    (parseInt(weight.value) / (totalHeight * totalHeight)) *
-    703
-  ).toFixed(2);
+  if (document.getElementById("bmi-imperial").checked) {
+    totalHeight =
+      parseInt(heightFeet.value) * 12 + parseInt(heightInches.value);
+    bmiNumber = (
+      (parseInt(weight.value) / (totalHeight * totalHeight)) *
+      703
+    ).toFixed(2);
+  } else {
+    totalHeight = parseInt(heightFeet.value)/100;
+    bmiNumber = (
+      (parseInt(weight.value) / (totalHeight * totalHeight)) 
+    ).toFixed(2);
+  }
 };
